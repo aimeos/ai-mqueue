2f546261807fa9b4a3867c2162014bd94451729d
Aimeos <aimeos@aimeos.org>
2021-06-18 09:20:40 +0200

    Minor coding style improvement

--- manifest.php
+++ manifest.php
@@ -1,11 +1,11 @@
 <?php
 
-return array(
+return [
 	'name' => 'ai-mqueue',
-	'depends' => array(
+	'depends' => [
 		'aimeos-core',
-	),
-	'include' => array(
+	],
+	'include' => [
 		'lib/custom/src',
-	),
-);
+	],
+];
