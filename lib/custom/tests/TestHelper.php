35be0e7618ab74c88bd498940e64f3fbc3f4add4
Aimeos <aimeos@aimeos.org>
2022-01-06 09:21:53 +0100

    Copyright update

--- lib/custom/tests/TestHelper.php
+++ lib/custom/tests/TestHelper.php
@@ -1,11 +1,11 @@
 <?php
 
-
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Metaways Infosystems GmbH, 2011
- * @copyright Aimeos (aimeos.org), 2015-2021
+ * @copyright Aimeos (aimeos.org), 2015-2022
  */
+
+
 class TestHelper
 {
 	private static $aimeos;
